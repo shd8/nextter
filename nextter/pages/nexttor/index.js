7d167415921e282a8ef6aa25dfdc9d2d21f0915d
Fernando Gomez <shadyfast8.services@gmail.com>
2021-07-07 22:38:33 +0200

    Create next app with basic navigation

---
+++ nextter/pages/nexttor/index.js
@@ -0,0 +1,10 @@
+import styles from '../../styles/Nexttor.module.css';
+
+export default function Nexttor() {
+    return (
+        <>
+            <h1 className={styles.h1}>This is nexttor</h1>
+            <h3 className={styles.header}>This is a h3</h3>
+        </>
+    )
+}
